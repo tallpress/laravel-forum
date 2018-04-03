87f073c46d30339c1ae51b48786e9220d7c0fb0f
tom <tomallpress@hotmail.com>
2018-04-03 13:28:20 +0100

    added activites model that records a users activities, such as posting a thread, and posting a reply

--- app/Thread.php
+++ app/Thread.php
@@ -6,52 +6,54 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-  protected $guarded = [];
-  protected $with = ['creator', 'channel'];
-  
-  protected static function boot()
-  {
-    parent::boot();
-    static::addGlobalScope('replyCount', function($builder){
-      $builder->withCount('replies');
-    });
-    static::deleting(function($thread){
-      $thread->replies()->delete();
-    });
-  }
+      use RecordsActivity;
+      
+      protected $guarded = [];
+      protected $with = ['creator', 'channel'];
 
-  public function replies()
-  {
-    return $this->hasMany(Reply::class);
-  }
+      protected static function boot()
+      {
+        parent::boot();
+        static::addGlobalScope('replyCount', function($builder){
+          $builder->withCount('replies');
+        });
+        static::deleting(function($thread){
+          $thread->replies()->delete();
+        });
+      }
 
-  public function channel()
-  {
-    return $this->belongsTo(Channel::class);
-  }
+      public function replies()
+      {
+        return $this->hasMany(Reply::class);
+      }
 
-  public function creator()
-  {
-    return $this->belongsTo(User::class, 'user_id');
-  }
+      public function channel()
+      {
+        return $this->belongsTo(Channel::class);
+      }
 
-
-  public function addReply($reply)
-  {
-    $this->replies()->create($reply)
-      -> withCount('favorites');
-  }
+      public function creator()
+      {
+        return $this->belongsTo(User::class, 'user_id');
+      }
 
 
-  public function path()
-  {
-    return "/threads/{$this->channel->slug}/{$this->id}";
-  }
+      public function addReply($reply)
+      {
+        $this->replies()->create($reply)
+          -> withCount('favorites');
+      }
 
 
-  public function scopeFilter($query, $filters)
-  {
-    return $filters->apply($query);
-  }
+      public function path()
+      {
+        return "/threads/{$this->channel->slug}/{$this->id}";
+      }
+
+
+      public function scopeFilter($query, $filters)
+      {
+        return $filters->apply($query);
+      }
 
 }
