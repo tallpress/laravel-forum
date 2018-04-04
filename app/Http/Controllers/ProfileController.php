ce36c6d3815c7a1177cee4568ac7469972ce26d2
tom <tomallpress@hotmail.com>
2018-04-04 10:02:22 +0100

    using activity feed function as a query to get the users recent activites, i.e. posts and replies

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,12 +8,10 @@ class ProfileController extends Controller
 {
   public function show(User $user)
   {
-    $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity) {
-        return $activity->created_at->format('Y-m-d');
-    });
+
     return view('profiles.show', [
         'profileUser' => $user,
-        'activities' => $activities
+        'activities' => \App\Activity::feed($user)
     ]);
   }
 }
--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Activity;
+use Carbon\Carbon;
 
 class ActiviyTest extends TestCase
 {
@@ -32,5 +33,23 @@ class ActiviyTest extends TestCase
         $this->signIn();
         $reply = create('App\Reply');
         $this->assertEquals(2, Activity::count());
+    }
+
+    /** @test */
+    public function it_fetches_a_feed_for_any_user()
+    {
+        $this->signIn();
+        $thread = create('App\Thread', ['user_id' => auth()->id()], 2);
+
+        auth()->user()->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+
+        $feed = Activity::feed(auth()->user());
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
     }
 }
